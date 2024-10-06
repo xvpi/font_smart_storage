265cad1c2532546364500cc9f0af9072803521c8
asd395 <2396484373@qq.com>
2024-10-06 23:04:46 +0800

    我的部分修改20241006

---
+++ pages/myinfo/account.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<u-cell-group>
+			<u-cell-item title="修改登录密码"></u-cell-item>
+			<u-cell-item title="更换手机号"></u-cell-item>
+			<u-cell-item title="更换邮箱"></u-cell-item>
+		</u-cell-group>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/myinfo/theme.vue
@@ -0,0 +1,29 @@
+<template>
+	<view style="display: flex; margin-left: 5px; margin-top: 20px;">
+		<view style="flex: 1; display: flex; flex-direction: column; align-items: center;">
+			<image src="/static/test/1.png" style="width: 150px; height: 250px;"></image>
+			<text style="margin-top: 10px;">xxx</text>
+		</view>
+		<view style="flex: 1; display: flex; flex-direction: column; align-items: center; margin-left: 1px;">
+			<image src="/static/test/2.png" style="width: 150px; height: 250px;"></image>
+			<text style="margin-top: 10px;">xxx</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
